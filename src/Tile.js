b1fd46bd532a289bd0fc72a7a78082492cc61a96
techspeterson <techspeterson@gmail.com>
2020-05-10 21:47:10 +1000

    grey out names on map when reports are collected

--- src/Tile.js
+++ src/Tile.js
@@ -1,17 +1,31 @@
 import React from "react";
+import { connect } from "react-redux";
+
+function mapStateToProps(state) {
+  return {
+    allPorts: state.ports
+  }
+}
 
 function Tile(props) {
   const ports = Object.entries(props.tile.ports);
+  const { allPorts } = props;
 
   return (
     <div>
       <ul>
         {ports.map((port, index) => {
-          return <li key={index}>{port[1].name}</li>
+          let collected = false;
+          const portObject = allPorts[port[0]];
+          if (portObject) {
+            collected = portObject.collected
+          }
+
+          return <li key={index} className={collected ? "collected" : ""}>{port[1].name}</li>
         })}
       </ul>
     </div>
   )
 }
 
-export default Tile;
+export default connect(mapStateToProps)(Tile);
